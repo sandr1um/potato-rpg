2750b482d89211f02a3cdd4fadad514b11612ec5
Alexander <alexander.ivanets@rwth-aachen.de>
2024-01-26 09:57:20 +0100

    Refactored GameHandler

--- src/main/java/rpg/potato/app/GameHandler.java
+++ src/main/java/rpg/potato/app/GameHandler.java
@@ -9,98 +9,102 @@ import static rpg.potato.app.Attribute.*;
 
 
 public class GameHandler {
-    private final EnumMap<Attribute, Integer> scores;
+    private final EnumMap<Attribute, Integer> gameStateScores;
+
     public GameHandler() {
-        this.scores = new EnumMap<>(Attribute.class);
-        this.scores.put(DESTINY, 0);
-        this.scores.put(POTATOES, 0);
-        this.scores.put(ORCS, 0);
-        this.scores.put(SCALING, 1);
+        this.gameStateScores = new EnumMap<>(Attribute.class);
+        this.gameStateScores.put(DESTINY, 0);
+        this.gameStateScores.put(POTATOES, 0);
+        this.gameStateScores.put(ORCS, 0);
+        this.gameStateScores.put(SCALING, 1);
     }
+
     public int getScore(Attribute attribute) {
-        return scores.get(attribute);
+        return gameStateScores.get(attribute);
     }
 
     public GameStateEntity startNewGame() {
-        scores.replaceAll((a, v) -> 0);
-        scores.put(SCALING, 1);
+        gameStateScores.replaceAll((a, v) -> 0);
+        gameStateScores.put(SCALING, 1);
 
-        return new GameStateEntity(
-                getScore(DESTINY),
-                getScore(POTATOES),
-                getScore(ORCS),
-                getScore(SCALING),
-                "New Game!");
+        GameStateEntity gameState = new GameStateEntity();
+        setGameStateValues(gameState, "New Game!");
+
+        return gameState;
     }
 
     public GameStateEntity applyEvent(Event event) {
+        GameStateEntity gameState = new GameStateEntity();
+        
         if (!isFinished()) {
             event.getModifiers().forEach(this::changeScore);
         }
-        return new GameStateEntity(
-                getScore(DESTINY),
-                getScore(POTATOES),
-                getScore(ORCS),
-                getScore(SCALING),
-                event.getMessage());
+        
+        setGameStateValues(gameState, event.getMessage());
+        
+        return gameState;
     }
 
     public GameStateEntity applyScaling() {
-        String message;
+        GameStateEntity gameState = new GameStateEntity();
         if (getScore(ORCS) == 0) {
-            message = "There are no orcs";
+            setGameStateValues(gameState, "There are no orcs");
         } else if (getScore(POTATOES) >= getScore(SCALING)) {
             changeScore(ORCS, -1);
             changeScore(POTATOES, -1 * getScaling());
-            message = "Removed one orc at the expense of " +
+            setGameStateValues(gameState, "Removed one orc at the expense of " +
                     getScaling() +
-                    (getScaling() == 1 ? " potato" : " potatoes");
+                    (getScaling() == 1 ? " potato" : " potatoes"));
+
         } else {
-            message = "Not enough potatoes";
+            setGameStateValues(gameState, "Not enough potatoes");
         }
 
-        return new GameStateEntity(
-                getScore(DESTINY),
-                getScore(POTATOES),
-                getScore(ORCS),
-                getScore(SCALING),
-                message
-        );
+        return gameState;
+    }
+
+    private void setGameStateValues(GameStateEntity gameState, String message) {
+        gameState.setDestiny(getScore(DESTINY));
+        gameState.setPotatoes(getScore(POTATOES));
+        gameState.setOrcs(getScore(ORCS));
+        gameState.setScaling(getScore(SCALING));
+        gameState.setMessage(message);
     }
 
     public boolean isFinished() {
-        return scores.keySet()
+        return gameStateScores.keySet()
                 .stream()
-                .anyMatch(item -> scores.get(item) == 10 && !item.equals(Attribute.SCALING));
+                .anyMatch(item -> gameStateScores.get(item) == 10 && !item.equals(Attribute.SCALING));
     }
 
     private void changeScore(Attribute attribute, int change) {
-        int changes = scores.get(attribute) + change;
+        int changes = gameStateScores.get(attribute) + change;
         if (changes >= 0) {
-            scores.put(attribute, changes);
+            gameStateScores.put(attribute, changes);
         }
     }
 
     public String generateFinalMessage() {
-        Attribute endAttribute = scores
+        Attribute endAttribute = gameStateScores
                 .keySet()
                 .stream()
-                .filter(item -> scores.get(item) == 10 && !item.equals(Attribute.SCALING))
+                .filter(item -> gameStateScores.get(item) == 10 && !item.equals(Attribute.SCALING))
                 .findAny()
                 .orElseThrow();
 
         return switch (endAttribute) {
-            case DESTINY ->"An interfering bard or wizard turns up at your doorstep with a quest," +
+            case DESTINY -> "An interfering bard or wizard turns up at your doorstep with a quest," +
                     " and you are whisked away against your will on an adventure.";
             case POTATOES -> "You have enough potatoes that you can go underground and not return to the surface" +
                     " until the danger is past. You nestle down into your burrow and enjoy your well earned rest";
-            case ORCS -> "Orcs have finally find your potato farm. Alas, orcs are not so interested in potatoes as they" +
-                    " are in eating you, and you end up in a cock pot";
+            case ORCS ->
+                    "Orcs have finally find your potato farm. Alas, orcs are not so interested in potatoes as they" +
+                            " are in eating you, and you end up in a cock pot";
             case SCALING -> "";
         };
     }
 
     public int getScaling() {
-        return scores.get(Attribute.SCALING);
+        return gameStateScores.get(Attribute.SCALING);
     }
 }
